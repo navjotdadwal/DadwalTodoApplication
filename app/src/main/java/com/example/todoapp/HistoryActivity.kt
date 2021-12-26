c3fefd4c2130c45778560aaf66a44785df5223d2
navjotdadwal <navjotsinghdadwal@gmail.com>
2021-12-26 11:21:09 +0530

    first commit

---
+++ app/src/main/java/com/example/todoapp/HistoryActivity.kt
@@ -0,0 +1,110 @@
+package com.example.todoapp
+
+import android.content.Intent
+import android.graphics.*
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class HistoryActivity : AppCompatActivity() {
+
+    val list = arrayListOf<TodoModel>()
+    var adapter = TodoAdapter(list)
+
+    val db by lazy {
+        AppDatabase.getDatabase(this)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history)
+        setSupportActionBar(toolbar)
+        todoRv.apply {
+            layoutManager = LinearLayoutManager(this@HistoryActivity)
+            adapter = this@HistoryActivity.adapter
+        }
+
+        db.todoDao().getTask().observe(this, Observer {
+            if (!it.isNullOrEmpty()) {
+                list.clear()
+                list.addAll(it)
+                adapter.notifyDataSetChanged()
+            }else{
+                list.clear()
+                adapter.notifyDataSetChanged()
+            }
+        })
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        val item = menu.findItem(R.id.search)
+        val searchView = item.actionView as SearchView
+        item.setOnActionExpandListener(object :MenuItem.OnActionExpandListener{
+            override fun onMenuItemActionExpand(item: MenuItem?): Boolean {
+                displayTodo()
+                return true
+            }
+
+            override fun onMenuItemActionCollapse(item: MenuItem?): Boolean {
+                displayTodo()
+                return true
+            }
+
+        })
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if(!newText.isNullOrEmpty()){
+                    displayTodo(newText)
+                }
+                return true
+            }
+
+        })
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    fun displayTodo(newText: String = "") {
+        db.todoDao().getTask().observe(this, Observer {
+            if(it.isNotEmpty()){
+                list.clear()
+                list.addAll(
+                    it.filter { todo ->
+                        todo.title.contains(newText,true)
+                    }
+                )
+                adapter.notifyDataSetChanged()
+            }
+        })
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.history -> {
+                startActivity(Intent(this, HistoryActivity::class.java))
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun openNewTask(view: View) {
+        startActivity(Intent(this, TaskActivity::class.java))
+    }
+}
